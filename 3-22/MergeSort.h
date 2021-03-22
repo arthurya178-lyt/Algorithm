03b607413b3db01761cd3131bef74436382fea26
arthurya178-pccu <59197410+arthurya178-pccu@users.noreply.github.com>
2021-03-22 01:58:03 -0800

    Create MergeSort.h

---
+++ 3-22/MergeSort.h
@@ -0,0 +1,11 @@
+#ifndef MergeSort_H
+#define MergeSort_H
+
+#include<vector>
+using namespace std;
+void merge_sort(vector<int> & A, size_t p, size_t r);
+void merge(vector<int> &arr, size_t f, size_t m, size_t e);
+
+#endif // !MergeSort_H
+
+
